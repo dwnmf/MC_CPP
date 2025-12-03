843bbe409fe41c129c0a5192d9c387dec204c397
Arta48 <nik_artamonov_2017@mail.ru>
2025-12-03 14:51:33 +0300

    compile.sh remaked

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 rm -rf build
 
 mkdir build && cd build
-cmake ..
-cmake --build .
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF ..
+cmake --build . --config Release --parallel
